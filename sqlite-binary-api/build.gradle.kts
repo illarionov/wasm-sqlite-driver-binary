11021319a531fc33b791d0fe212c814ba2a901a8
Alexey Illarionov <alexey@0xdc.ru>
2024-06-22 17:16:31 +0300

    Add linux target to sqlite-binary-api (#4)

--- sqlite-binary-api/build.gradle.kts
+++ sqlite-binary-api/build.gradle.kts
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2024, the wasm-sqlite-open-helper project authors and contributors. Please see the AUTHORS file
+ * for details. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
     id("ru.pixnews.wasm.sqlite.binary.gradle.multiplatform.kotlin")
@@ -12,4 +21,19 @@ version = wasmSqliteVersions.getSubmoduleVersionProvider(
 
 kotlin {
     jvm()
+    js {
+      browser()
+      nodejs()
+    }
+    wasmJs {
+        browser()
+        nodejs()
+    }
+    iosSimulatorArm64()
+    iosX64()
+    linuxArm64()
+    linuxX64()
+    macosArm64()
+    macosX64()
+    mingwX64()
 }
