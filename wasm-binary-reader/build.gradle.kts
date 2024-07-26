7324d2a097495a3f6ca434c3c6be3fff9eef14e3
Alexey Illarionov <alexey@0xdc.ru>
2024-07-26 15:09:47 +0300

    Add test for WasmSourceReader.readOrThrow (#28)

--- wasm-binary-reader/build.gradle.kts
+++ wasm-binary-reader/build.gradle.kts
@@ -36,12 +36,20 @@ kotlin {
     linuxX64()
     macosArm64()
     macosX64()
-    mingwX64()
+    mingwX64 {
+        binaries.all {
+            linkerOpts("-lole32")
+        }
+    }
 
     sourceSets {
         commonMain.dependencies {
             api(projects.sqliteBinaryApi)
             api(libs.kotlinx.io)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(libs.assertk)
         }
         nativeMain.dependencies {
             implementation(projects.commonXdg)
